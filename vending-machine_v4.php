06c4b15f68913e27c286f2d6cb270da43829161a
guztus <gustavs.zubars@gmail.com>
2022-11-06 21:53:41 +0200

    added all versions

---
+++ vending-machine_v4.php
@@ -0,0 +1,146 @@
+<?php
+
+// insert coins
+// choose product
+//
+// product has price
+// we give 5 eur
+// we have to get remainder back in 'monetas' - starting from the biggest
+//
+// 40 min time
+// extra --
+//
+// use objects (arrays)
+
+// 1.
+// User can input money until they can pay for it
+// Check if input is valid
+//
+// 2.
+// Cash value count in machine
+
+function formatMoney(int $amount):string {
+    return ($amount / 100) . "$";
+}
+
+$products = [
+    ['name' => 'Hot Chocolate', 'cost' => 121],
+    ['name' => 'Cappuccino', 'cost' => 180],
+    ['name' => 'Tea', 'cost' => 120]
+];
+
+$coins = [
+    ['value' => 200, 'count' => 50],
+    ['value' => 100, 'count' => 51],
+    ['value' => 50, 'count' => 52],
+    ['value' => 20, 'count' => 53],
+    ['value' => 10, 'count' => 54],
+    ['value' => 5, 'count' => 55],
+    ['value' => 2, 'count' => 10],
+    ['value' => 1, 'count' => 50],
+];
+
+
+while (true) {
+
+    echo "Please choose your product by entering a number!\n";
+
+    $productCount = 0;
+
+    foreach ($products as $product) {
+        $productCost = $product['cost'] / 100;
+        $productCount++;
+        echo "{$productCount} - {$product['name']} (\${$productCost})\n";
+    }
+
+    $userChoice = (int)readline();
+
+//    make cases for if userChoice is not an int
+
+    $userCash = 0;
+
+    $remainder = 0;
+
+    $productKeys = array_keys($products);
+
+
+    $productFound = 0;
+    $productChosen = 0;
+    foreach ($productKeys as $productKey) {
+        if ($userChoice == $productKey + 1) {
+            $remainder = $userCash - $products[$productKey]['cost'];
+            echo "Chosen product is {$products[$productKey]['name']}\n";
+            $productFound++;
+
+            $productChosen = $productKey;
+            break;
+        }
+    }
+
+    $remainder = abs($remainder);
+
+
+    if ($productFound == 0) {
+        continue;
+    }
+
+    $inputCoinsInWords = '';
+
+    while ($userCash < $products[$productChosen]['cost']) {
+
+        echo "Please enter coins! :)\n";
+        $userInputCoins = (int)readline();
+
+        $coinKeys = array_keys($coins);
+
+        $coinIsValid = 0;
+        $coinChosenId = 0;
+        foreach ($coinKeys as $coinKey) {
+            if ($userInputCoins == $coins[$coinKey]['value']) {
+
+                $coinIsValid++;
+
+                $coinChosenId = $coinKey - 1;
+                break;
+            }
+
+        }
+
+        if ($coinIsValid == 1) {
+            $userCash += $userInputCoins;
+
+            $userInputInCorrectValue = ($userInputCoins / 100);
+            $inputCoinsInWords .= " \${$userInputInCorrectValue}";
+            $coins[$coinChosenId]['count']++;
+        }
+
+        echo "---- ----\n";
+        echo "You have input -{$inputCoinsInWords}\n";
+        echo "---- ----\n";
+    }
+
+    $remainder = $userCash - $remainder;
+
+    $outputMoney = '';
+    $outputMoneyTEST = 0;
+
+    $totalRemainder = $remainder;
+
+    while ($remainder > 0) {
+        for ($i = 0; $i < count($coins); $i++) {
+            if ($remainder - $coins[$i]['value'] >= 0) {
+                if ($coins[$i]['count'] > 0) {
+                    $remainder -= $coins[$i]['value'];
+
+                    $remainderInCash = ($coins[$i]['value'] / 100);
+
+                    $outputMoney .= "\${$remainderInCash} ";
+                }
+            }
+        }
+    }
+
+    echo "Here's your change! {$outputMoney} ({$totalRemainder})\n";
+
+    exit;
+}
